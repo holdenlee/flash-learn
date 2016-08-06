302b6f8516c074aaf6c6094d306dc491550fc5ee
Holden Lee <holdenl@princeton.edu>
2016-08-06 19:04:04 -0400

    view

---
+++ View.elm
@@ -0,0 +1,47 @@
+module View exposing (makePage, DisplayThing (..))
+
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+import Random
+import Dict as D
+import List exposing (..)
+import Maybe as M
+import Debug exposing (..)
+import Keyboard as K
+import Time exposing (..)
+import Char exposing (..)
+import Platform.Sub as S
+import Task exposing (perform)
+import Time exposing (..)
+import Random exposing (generate, float)
+import Platform.Cmd as C
+import Html.Attributes exposing (..)
+
+type DisplayThing = Text String | Image String
+
+type Msg = TextChanged String | EnterPressed | TopPressed | BottomPressed | Save
+
+makePage : DisplayThing -> DisplayThing -> Float -> Float -> Html Msg
+makePage card1 card2 avg speed =
+  table [class "center", attribute "style" "width:100%"] --2x2, consisting of cards, side pane, text input
+      [tr [] --cards and side pane
+           [td [] --cards
+                [table [attribute "style" "width:100%;height:100%"
+                       ] -- attribute "border" "1" -- nested table for cards
+                    [tr [] [td [class "index_card"] [text "1"]],
+                     tr [] [td [class "index_card"] [text "1"]]]],
+--                     [text "left"]],
+{-                     [col []
+                          [td [class "index_card"] [text "1"],
+                           td [class "index_card"] [text "2"]]]],-}
+            td [] --side panel
+                [p [] [text "Average time:"],
+                 p [class "center"] [text (toString avg)],
+                 p [] [text "Speed (per minute):"],
+                 p [class "center"] [text (toString speed)],
+                 button [onClick TopPressed] [text "top"],
+                 br [] [],
+                 button [onClick BottomPressed] [text "top"]]],
+                 --text "side"]],
+            tr [] [td [colspan 2] [input [onInput TextChanged, attribute "style" "width:100%"] []]]]
---
+++ ViewTest.elm
@@ -0,0 +1,5 @@
+module ViewTest exposing (..)
+
+import View exposing (..)
+
+main = makePage (Text "1") (Text "2") 0 0
